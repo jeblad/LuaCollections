a2133a71b0ea6d0ccc7e3b869c56ef27c7248712
John Erling Blad <jeblad@gmail.com>
2019-09-12 19:55:47 +0200

    Update util.lua

--- util.lua
+++ util.lua
@@ -5,10 +5,15 @@
 -- @var Table holding the modules exported members
 local util = {}
 
---- Raw count of all the items in the provided table.
--- Variant of
+--- Raw table count of all the items.
+-- Simplified variant of
 -- ['count()'](https://github.com/Yonaba/Moses/blob/master/moses.lua#L308-L322)
--- from [Yonaba/Moses](https://github.com/Yonaba/Moses).
+-- ([doc](http://yonaba.github.io/Moses/doc/#count))
+-- from [Yonaba: Moses](http://yonaba.github.io/Moses/).
+-- Approximation of
+-- ['size()'](https://github.com/jashkenas/underscore/blob/master/underscore.js#L481-L485)
+-- ((doc)(https://underscorejs.org/#size))
+-- from [jashkenas: Underscore.js](https://underscorejs.org/)
 -- @tparam table t has its entries counted
 -- @treturn number raw entries
 function util.count( t )
@@ -19,10 +24,11 @@ function util.count( t )
 	return i
 end
 
---- Size based on the raw count.
+--- Size based on the raw table count, or vararg count.
 -- Variant of
 -- ['size()'](https://github.com/Yonaba/Moses/blob/master/moses.lua#L802-L813)
--- from [Yonaba/Moses](https://github.com/Yonaba/Moses).
+-- ((doc)[http://yonaba.github.io/Moses/doc/#size])
+-- from [Yonaba: Moses](http://yonaba.github.io/Moses/).
 -- @tparam[opt] table|any count entries if table, count varargs otherwise
 -- @treturn number counted entries
 function util.size( ... )
@@ -80,9 +86,22 @@ function util.isMetatableEqual( a, b )
 end
 
 --- Deep equal of two objects.
--- Variant of
+-- Simplified variant of
 -- ['isEqual()'](https://github.com/Yonaba/Moses/blob/master/moses.lua#L2775-L2815)
--- from [Yonaba/Moses](https://github.com/Yonaba/Moses).
+-- ((doc)[http://yonaba.github.io/Moses/doc/#isEqual])
+-- from [Yonaba: Moses](http://yonaba.github.io/Moses/).
+-- Variant of
+-- ['is_equal()'](https://github.com/mirven/underscore.lua/blob/master/lib/underscore.lua#L328-L352)
+-- ((doc)[https://mirven.github.io/underscore.lua/#is_equal])
+-- from [mirven: Underscore.lua](https://mirven.github.io/underscore.lua/).
+-- Approximation of
+-- ['isEqual()'](https://github.com/jashkenas/underscore/blob/master/underscore.js#L1295-L1298)
+-- ((doc)(https://underscorejs.org/#isEqual))
+-- from [jashkenas: Underscore.js](https://underscorejs.org/)
+-- Variant of
+-- ['deepcompare()'](https://github.com/stevedonovan/Penlight/blob/master/lua/pl/tablex.lua#L154-L64)
+-- ((doc)[http://stevedonovan.github.io/Penlight/api/libraries/pl.tablex.html#deepcompare])
+-- from [Penlight: pl.tablex.lua](http://stevedonovan.github.io/Penlight/api/libraries/pl.tablex.html).
 -- @param a any type of object
 -- @param b any type of object
 -- @tparam[opt=false] boolean useMt indicator for whether to include the meta table
@@ -121,14 +140,23 @@ function util.deepEqual( a, b, useMt )
 	return true
 end
 
---- Checks if a table contains the arg.
--- Variant of
+--- Checks if a table include the arg.
+-- Simplified variant of
 -- ['include()'](https://github.com/Yonaba/Moses/blob/master/moses.lua#L526-L541)
--- from [Yonaba/Moses](https://github.com/Yonaba/Moses).
+-- ((doc)[http://yonaba.github.io/Moses/doc/#include])
+-- from [Yonaba: Moses](http://yonaba.github.io/Moses/).
+-- Simplified variant (defunc) of
+-- ['include()'](https://github.com/mirven/underscore.lua/blob/master/lib/underscore.lua#L150-L155)
+-- ((doc)[https://mirven.github.io/underscore.lua/#include])
+-- from [mirven: Underscore.lua](https://mirven.github.io/underscore.lua/).
+-- Approximation of
+-- ['contains()'](https://github.com/jashkenas/underscore/blob/master/underscore.js#L291-L297)
+-- ((doc)(https://underscorejs.org/#contains))
+-- from [jashkenas: Underscore.js](https://underscorejs.org/)
 -- @tparam table t searched for the arg
 -- @param arg any item to be searched for
 -- @treturn boolean result of the operation
-function util.contains( t, arg )
+function util.include( t, arg )
 	-- inlined code from '_.toBoolean' and '_.detect' from "Moses"
 	local cmp = ( type( arg ) == 'function' ) and arg or util.deepEqual
 	for k,v in pairs( t ) do
@@ -138,6 +166,7 @@ function util.contains( t, arg )
 	end
 	return false
 end
+util.contains = util.include
 
 -- return the export table
 return util
