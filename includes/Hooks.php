e3099c72f7192f44742a6b9682a4e06cf349a516
John Erling Blad <jeblad@gmail.com>
2019-10-19 21:56:36 +0200

    Added Hooks.php

---
+++ includes/Hooks.php
@@ -0,0 +1,45 @@
+<?php
+
+declare( strict_types = 1 );
+
+namespace Collections;
+
+/**
+ * Hook handlers for the Collections extension
+ *
+ * @ingroup Extensions
+ */
+
+class Hooks {
+
+	/**
+	 * Setup for the extension
+	 */
+	public static function onExtensionSetup() {
+		global $wgDebugComments;
+
+		// turn on comments while in development
+		$wgDebugComments = true;
+	}
+
+	/**
+	 * External Lua library paths for Scribunto
+	 *
+	 * @param any $engine to be used for the call
+	 * @param array &$extraLibraryPaths additional libs
+	 * @return bool
+	 */
+	public static function onRegisterScribuntoExternalLibraryPaths(
+		string $engine,
+		array &$extraLibraryPaths
+	): bool {
+		if ( $engine !== 'lua' ) {
+			return true;
+		}
+
+		// Path containing pure Lua libraries that don't need to interact with PHP
+		$extraLibraryPaths[] = __DIR__ . '/LuaLibrary/lua/pure';
+
+		return true;
+	}
+}
