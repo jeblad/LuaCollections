f619113be77983a2e4894d236bbb14622e7483e3
John Erling Blad <jeblad@gmail.com>
2019-11-08 18:57:45 +0100

    Modified stack

--- includes/LuaLibrary/lua/pure/stack.lua
+++ includes/LuaLibrary/lua/pure/stack.lua
@@ -1,12 +1,17 @@
 --- Class for a stack.
--- This is an open structure, to make it easilly extensible.
+-- This is a semi-open structure, to make it easy to extend and adapt.
 -- That is, the stack can be abused by manipulating it as an ordinary table.
 -- Items can be added to one end of a queue, and later removed from the same end.
 -- See also Wikipedias page on [stack](https://en.wikipedia.org/wiki/Stack_(abstract_data_type)).
 -- @module Stack
 
 -- pure libs
-local util = require 'collectionUtil'
+local colUtil = require 'collectionUtil'
+local libUtil = require 'libraryUtil'
+
+-- lookup
+local checkType = libUtil.checkType
+local makeCheckSelfFunction = libUtil.makeCheckSelfFunction
 
 -- @var metatable for stacks
 local stack = {}
@@ -14,57 +19,120 @@ local stack = {}
 -- loop indexing back to metatable
 stack.__index = stack
 
---- Create a new instance.
--- @function stack.new
+-- called when …
+function stack.__call( t, value )
+	if value then
+		table.insert( t, value )
+	end
+
+	return t[#t]
+end
+
+--- Internal creator function.
+-- @local
 -- @tparam vararg ... arguments to be collected
 -- @treturn self
-function stack.new( ... )
-	local new = setmetatable( {}, stack )
+local function makeStack( ... )
+	local inner = setmetatable( {}, stack )
+	inner.__index = inner
+	local outer = setmetatable( {}, inner )
+
+	--- Check whether method is part of self.
+	-- @local
+	-- @function checkSelf
+	-- @raise if called from a method not part of self
+	local checkSelf = makeCheckSelfFunction( 'stack', 'obj', outer, 'stack object' )
+
+	-- keep in closure
+	local _length = 0
+
+	-- keep in exposed structure
 	for i,v in ipairs{ ... } do
-		new[i] = v
+		_length = _length + 1
+		outer[i] = v
 	end
-	return new
+
+	--- Push items on the stack.
+	-- @function stack:push
+	-- @tparam vararg ...
+	-- @treturn self
+	function inner:push( ... )
+		checkSelf( self, 'push' )
+
+		for _,v in ipairs{ ... } do
+			_length = _length + 1
+			table.insert( self, v )
+		end
+
+		return self
+	end
+
+	--- Pop one or more items off the stack.
+	-- @function stack:pop
+	-- @tparam number n levels to be popped
+	-- @treturn varag
+	function inner:pop( n )
+		checkSelf( self, 'pop' )
+		checkType( 'stack:pop', 1, n, 'number', true )
+
+		local t = {}
+		for i = 1, ( n or 1 ) do
+			_length = _length - 1
+			t[i] = table.remove( self )
+		end
+
+		if not n then
+			return unpack( t )
+		end
+
+		return t
+	end
+
+	--- Drop one or more items off the stack.
+	-- @function stack:drop
+	-- @tparam number n levels to be dropped
+	-- @treturn self
+	function inner:drop( n )
+		checkSelf( self, 'drop' )
+		checkType( 'stack:drop', 1, n, 'number', true )
+
+		for i = 1, ( n or 1 ) do
+			_length = _length - 1
+			table.remove( self )
+		end
+
+		return self
+	end
+
+	--- Length of structure.
+	-- @function stack:length
+	-- @treturn number
+	function inner:length()
+		checkSelf( self, 'length' )
+		return _length
+	end
+
+	return outer
 end
 
---- Push items on the stack.
--- @function stack:push
--- @tparam vararg ...
+--- Create a new instance.
+-- @function stack.new
+-- @tparam vararg ... arguments to be passed on
 -- @treturn self
-function stack:push( ... )
-	for _,v in ipairs{ ... } do
-		table.insert( self, v )
-	end
-	return self
+function stack.new( ... )
+	return makeStack( ... )
 end
-
---- Pop one or more items off the stack.
--- @function stack:pop
--- @nick drop
--- @tparam number n levels to be popped
--- @treturn varag
-function stack:pop( n )
-	local t = {}
-	for i = 1, ( n or 1 ) do
-		t[i] = table.remove( self )
-	end
-	return unpack( t )
-end
-stack.drop = stack.pop
 
 --- Is the stack empty.
 -- Checks by counting items whether stack is empty.
 -- @function stack:isEmpty
 -- @treturn boolean
-function stack:isEmpty()
-	return util.empty( self )
-end
+stack.isEmpty = colUtil.empty
 
 --- Count number of entries in stack.
 -- @function stack:count
 -- @treturn number
-function stack:count()
-	return util.count( self )
-end
+stack.count = colUtil.count
 
 -- Return the final lib
 return stack
